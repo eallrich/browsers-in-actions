317172cead9cd302a6821ddf27d65095c4f5bbbe
Evan Allrich <evan@unguku.com>
2020-07-14 08:01:04 -0500

    Capture screenshots in FHD

--- screenshot.js
+++ screenshot.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.setViewport({width: 1024, height: 768});
+  await page.setViewport({width: 1920, height: 1080});
 
   await page.goto('https://www.reddit.com/');
   await page.screenshot({path: 'screenshot.png'});
